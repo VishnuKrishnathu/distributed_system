bd34535951f10b6a4f568db8f348be0f9682e4e2
Vishnu Krishnathu <54522897+VishnuKrishnathu@users.noreply.github.com>
2026-01-07 00:24:58 +0530

    decoder logic added

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -23,27 +23,30 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+type TCPTransportOpts struct {
+	ListenAddr    string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+}
+
 type TCPTransport struct {
-	listenAddress string
+	TCPTransportOpts
 	listener      net.Listener
-	shakeHands HandshakeFunc
-	decoder       Decoder
 
 	mu    sync.RWMutex
 	peers map[net.Addr]Peer
 }
 
-func NewTCPTransport(listenAddr string) *TCPTransport {
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
 	return &TCPTransport{
-		shakeHands: NOPHandshakeFunc,
-		listenAddress: listenAddr,
+		TCPTransportOpts: opts,
 	}
 }
 
 func (t *TCPTransport) ListenAndAccept() error {
 	var err error
 
-	t.listener, err = net.Listen("tcp", t.listenAddress)
+	t.listener, err = net.Listen("tcp", t.ListenAddr)
 
 	if err != nil {
 		return err
@@ -72,7 +75,11 @@ type Temp struct{}
 func (t *TCPTransport) handleConn(conn net.Conn) {
 	peer := NewTCPPeer(conn, true)
 
-	if err := t.shakeHands(peer); err != nil {
+	if err := t.HandshakeFunc(peer); err != nil {
+		conn.Close()
+
+		fmt.Printf("TCP error: %s\n", err)
+		return
 	}
 
 	msg := &Temp{}
@@ -80,7 +87,7 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
 
 	// Read loop
 	for {
-		if err := t.decoder.Decode(conn, msg); err != nil {
+		if err := t.Decoder.Decode(conn, msg); err != nil {
 			lenDecodeError++
 
 			if lenDecodeError > 5 {
